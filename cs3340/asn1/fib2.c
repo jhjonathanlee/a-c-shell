00e9a5bd56cc8845b305ff4bc46bf0f63518a1f0
Jonathan Lee <jh.jonathanlee@gmail.com>
2016-01-29 04:09:25 +0000

    adding big int #1

--- cs3340/asn1/fib2.c
+++ cs3340/asn1/fib2.c
@@ -16,9 +16,9 @@ void expand(int *a, int size) {
 
 BigInteger *add(BigInteger *a, BigInteger *b) {
   BigInteger *retval = (BigInteger *) malloc(sizeof(BigInteger));
-
   BigInteger *biggerInt = (a->digits >= b->digits) ? a : b;
   BigInteger *smallerInt = (a->digits > b->digits) ? b : a;
+  retval->size = biggerInt->size;
   
   int *arr = (int *) malloc(sizeof(int)*biggerInt->size);
   
@@ -37,20 +37,48 @@ BigInteger *add(BigInteger *a, BigInteger *b) {
   }
 
   if (n2 > 0) {
-    if (i <= biggerInt->size)
+    if (i <= biggerInt->size) {
       expand(arr, biggerInt->size);
       retval->size = biggerInt->size*2;
+    }
+    do {
+      arr[i] = biggerInt->num[i] + n2;
+      if (arr[i] > 10) {
+        arr[i] -= 10;
+        n2 = 1;
+      } else {
+        n2 = 0;
+      }
+      i++;
+    } while (n2 > 0);
+  }
 
+  while (i < biggerInt->digits) {
+    arr[i] = biggerInt->nums[i];
+    i++;
+  }
+  retval->num = arr;
+  retval->digits = i;
+  return retval;
 }
 
 int main() {
   BigInteger *bigInt =(BigInteger *) malloc(sizeof(BigInteger));
+  BigInteger *bitInt2 = (BigInteger *) malloc(sizeof(BigInteger));
+  
   bigInt->digits = 1;
   bigInt->num = (int *) malloc(sizeof(int));
-  *bigInt->num = 0;
-  bigInt->size = 10;
+  *bigInt->num = 1;
+  bigInt->size = 1;
 
-  printf("%d\n", *bigInt->num);
+  bigInt2->digits = 1;
+  bigInt2->num = (int *) malloc(sizeof(int));
+  *big2->num = 8;
+  bigInt->1;
+
+  BigInteger* added = add(bigInt2, bigInt2);
+
+  printf("%d\n", *added->num);
 
   free(bigInt->num);
   free(bigInt);
