839cd472d31d096811c2061fd88148cbde1d733e
Jonathan Lee <jh.jonathanlee@gmail.com>
2016-02-08 21:44:00 +0000

    modified option_parsing

--- cs3305/asn1/option_parser.c
+++ cs3305/asn1/option_parser.c
@@ -5,25 +5,46 @@
 
 csh_cmd *get_options(char *s) {
   csh_cmd *cmd = malloc(sizeof(csh_cmd));
-
-  char **buf = malloc(sizeof(char *) * 32);
-  char *token;
-  int i = 0;
-  const char delim[2] = " ";
-
-  token = strtok(s, " \n");
-  asprintf(&(cmd->cmd), "%s", token);
-
-  while (token != NULL) {
-    asprintf(&(buf[i]), "%s", token);
-    token = strtok(NULL, " \n");
-    i++;
-  }
-
-  buf[i] = (char *) NULL;
-
+  
+  int i = make_tokenlist(s, cmd->options);
   cmd->num = i;
-  cmd->options = buf;
 
   return cmd;
+}
+
+int make_tokenlist(char *buf, char *tokens[]) {
+  char input_line[MAX];
+  char *line;
+  int i, n;
+
+  i = 0;
+
+  line = buf;
+
+  tokens[i] = strtok(line, " ");
+  do {
+    i++;
+    line = NULL;
+    tokens[i] = strtok(line, " ");
+  } while (tokens[i] != NULL);
+
+  return i;
+}
+// test handle
+void main(void) {
+  char input_line[MAX];
+  csh_cmd *cmd;
+  int i;
+  printf("vagrant> ");
+  if (fgets(input_line, MAX, stdin) != NULL) {
+    cmd = get_options(input_line);
+  }
+  else {
+    printf("failed? \n");
+  }
+
+  for (i = 0; i < cmd->num; i++) {
+    printf("token is %s\n", cmd->options[i]);
+  }
+  free(cmd);
 }
--- cs3305/asn1/option_parser.h
+++ cs3305/asn1/option_parser.h
@@ -1,12 +1,14 @@
 #ifndef option_parser_h
 #define option_parser_h
 
+#define MAX 256
+
 typedef struct {
-  char *cmd;
-  char **options;
+  char *options[MAX];
   int num;
 } csh_cmd;
 
 csh_cmd *get_options(char *s);
+int make_tokenlist(char *buf, char *tokens[]);
 
 #endif
